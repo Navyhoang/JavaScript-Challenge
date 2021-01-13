8dec7c2ee05fab887d00900c5836ab3494d59137
Navyhoang <69599135+Navyhoang@users.noreply.github.com>
2021-01-13 10:30:52 -0600

    Files Set Up

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,19 @@
+// connect to data.js file and set it to a variable
+var tableData = data;
+
+// Use the index.html file provided 
+// Append a table to your web page 
+// Columns: date/time, city, state, country, shape, and comment
+
+
+
+// Add new rows of data for each UFO sighting
+
+
+
+
+// Use a date form in your HTML document
+
+
+//write JavaScript code that will listen for events and search through the date/time column
+// to find rows that match user input.
