301858020cb64a7f86b77c42b0972251a5571ccc
Navyhoang <69599135+Navyhoang@users.noreply.github.com>
2021-01-13 15:20:34 -0600

    Commit 1 - UFO 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,19 +1,89 @@
 // connect to data.js file and set it to a variable
 var tableData = data;
 
-// Use the index.html file provided 
-// Append a table to your web page 
-// Columns: date/time, city, state, country, shape, and comment
+// Step 1: Append a table to the web page
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Use d3 to append table rows `tr`
+data.forEach(function(tableData) {
+
+    // Add new rows of data for each UFO sighting
+    var row = tbody.append("tr");
+  
+    // Loop through each object in the data array, keys and values to each column
+    Object.entries(tableData).forEach(function([key, value]) {
+
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+
+    });
+  });
+
+// Step 2: Filter the table data based on user's input
+// Use d3 to Select the form
+var form = d3.select("#form-group");
+
+// Create event handler and run the function below when user enters an input
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var dateForm = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = dateForm.property("value");
+
+    //Listen to the event (input is submited) and search through the date/time column
+    // to find rows that match user input.
+
+    var filteredData = tableData.filter(city => city.datetime === inputValue);
+
+    // Only display the filteredData in the table section
+    data.forEach(function(filteredData) {
+
+        // Delete all existing rows
+        d3.selectall("tr").remove()
+
+        // Add new rows of data for each UFO sighting
+        var row = tbody.append("tr");
+      
+        Object.entries(filteredData).forEach(function([key, value]) {
+    
+          // Append a cell to the row for each value
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+};
 
 
 
-// Add new rows of data for each UFO sighting
+// use index to access array items
+// use push to add item to array
+// use slice to return selected item of array
+// use join to add items of an array into a string
+// use split to turn a string into an array
+// use for loop or arrow function (2.06, 2.08)
+// use map to return a new array (2.05, 2.08)
+// use forEach to loop through an array and log each time (2.05)
+// use math operations
+// use filter (2.10)
+// --------------
+// us d3.select id or class, .text to modify text,. attr to get html, change href,
+// Select all list items, then change their font color
+// d3.selectAll("li").style("color", "blue");     change style
+// var li2 = d3.select("ul").append("li").text("Another new item!"); (3.01)
+//d3.select("table")   select table
+// add items to table (3.02, 3.03)
+// var inputText = d3.event.target.value; handle events (3.05)
 
 
 
 
-// Use a date form in your HTML document
-
-
-//write JavaScript code that will listen for events and search through the date/time column
-// to find rows that match user input.
